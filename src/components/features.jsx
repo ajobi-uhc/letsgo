f9e63725a3822105416faaf5b8e9c890248280ae
ajobi-uhc <aryajakkli2002@gmail.com>
2021-09-27 21:43:35 -0600

    'second comit'

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -7,7 +7,7 @@ export const Features = (props) => {
         <div className='col-md-10 text-white col-md-offset-1 section-title'>
           <h2 id = "pleasework">Features</h2>
           <br />
-          <h4>Founding StarGardens are not only the key to your own land, they are a direct communication channel between you and the story-world</h4><br />
+          <h4 id = "pleasework">Founding StarGardens are not only the key to your own land, they are a direct communication channel between you and the story-world</h4><br />
           <h4 id = "pleasework">Each founding StarGarden has unique traits lying within their metadata represented by an emblem that appears on the StarGarden and demarcates which Faction founders belong to.</h4>
           
           
@@ -22,17 +22,10 @@ export const Features = (props) => {
           <h3 id = "pleasework">StarGarden Founders will receive pieces of the game and story will be airdropped to them, depending on the make-up of these traits- so the faction you are in is important </h3><br />
           <h4 id = "pleasework">Dormant traits are another kind of easter egg that will reveal themselves to you as we move forward </h4>
         </div>
+        
         <div className='row'>
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-6'>
-                  {' '}
-                  <img src = "img/crazy.png"></img>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
-            : 'Loading...'}
+        <img src = "img/crazy.png"></img>
+        
         </div>
       </div>
     </div>
