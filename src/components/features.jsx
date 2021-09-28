cda263595b33fdb724112eb652e466e866e9a69e
ajobi-uhc <aryajakkli2002@gmail.com>
2021-09-27 22:40:14 -0600

    '3'

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,4 +1,4 @@
-
+import styles from './styles.css'
 
 export const Features = (props) => {
   return (
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,4 +1,4 @@
-
+import styles from './styles.css'
 
 export const Navigation = (props) => {
   return (
